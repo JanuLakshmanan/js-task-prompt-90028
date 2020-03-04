62eff1af6b257e9c024251b1fd764535d019f3d3
JanuLakshmanan <azhagujanu@gmail.com>
2020-03-04 17:32:14 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
   
   var person = prompt("Please enter your name");
-
-  while(person != null) {
+  if(person != null) {
     
     var start = prompt("Enter the start index");
     
-    while((!isNaN(start)) && start<=person.length && start>=0)
+    if((!isNaN(start)) && start<=person.length && start>=0)
     {
     var end = prompt("Enter the end index");
     
-       while((!isNaN(end))&& end<=person.length && end>start)
+       if((!isNaN(end))&& end<=person.length && end>start)
         {
            alert(person.slice(start,end));
-           break;
+          
          }
-         alert("Enter valid number");
+         else{
+            alert("Enter valid number");
+         }
+        
     } 
-       alert("Enter valid number"); 
-       
+    else{
+      alert("Enter valid number");
+    }
+          
        }
   alert("Enter the name");
+
+
   
 
 
